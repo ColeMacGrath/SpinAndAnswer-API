5260c3639ae042392b1d0855ec29b39351975959
ColeMacGrath <kernel_linux@outlook.com>
2018-09-19 21:25:39 -0500

    Created getAllUsers, getUser, changeUSerDAta methods in index.js for Middlewares.

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,4 +1,52 @@
-exports.addDate = (req, res, next) => {
-  req.body.date = Date.now();
-  next();
+//users
+exports.getAllUsers = (req, res, next) => {
+  const users = {
+    id: 0,
+    name: 'Initial Name',
+    email: 'mail',
+    username: 'username',
+    password: 'password',
+  };
+
+  const json = {
+    response: 'ok',
+    data: users,
+    total: 1,
+  };
+
+  res.send(json);
+}
+
+exports.getUser = (req, res, next) => {
+  const user = {
+    id: 0,
+    name: 'Initial Name',
+    email: 'mail',
+    username: 'username',
+    password: 'password',
+  };
+
+  const json = {
+    response: 'ok',
+    data: user,
+  };
+
+  res.send(json);
+}
+
+exports.changeUserData = (req, res, next) => {
+  const user = {
+    id: 0,
+    name: 'Initial Name',
+    email: 'mail',
+    username: 'username',
+    password: 'password',
+  };
+
+  const json = {
+    response: 'ok',
+    data: user,
+  };
+
+  res.send(json);
 }
