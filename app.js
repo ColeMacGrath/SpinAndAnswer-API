a09a00c5e08830c931271883bf5b594f6cdacb9f
Alejandra <rolesaji@gmail.com>
2018-09-22 11:14:18 -0500

    Se creó la conexión con MySql

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
 const router = require('./Route');
 
 app.use(router);
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "prueba"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+});
+
+con.query("SELECT * FROM usuarios", function (err, result, fields) {
+  if (err) throw err;
+  console.log(result)
+});
 
 app.listen(3000, () => console.log('running'));
