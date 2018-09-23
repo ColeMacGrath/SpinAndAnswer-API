4cec681721d0a89f7822e509caa44fb42b6e3db1
ColeMacGrath <kernel_linux@outlook.com>
2018-09-22 21:50:09 -0500

    Created factory and controllers structure

---
+++ Controllers/users.js
@@ -0,0 +1,28 @@
+/*class UserController {
+  getAll(req, res) {
+    const users = [
+      {
+        user_id: 1,
+        username: 'Juan',
+        name: 'juan',
+        admin: 0,
+        email: 'mail@mail.com',
+      },
+      {
+        user_id: 2,
+        username: 'Pedro',
+        name: 'pedro',
+        admin: 0,
+        email: 'mail2@mail2.com',
+      },
+    ];
+    const json = {
+      response: 'Ok',
+      data: users,
+      total: 2,
+    }
+    res.send(json);
+  }
+}
+
+module.exports = new UsersController();*/
