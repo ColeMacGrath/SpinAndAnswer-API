4864284d3f8cb988c4ea5155af0f216196c734ad
ColeMacGrath <kernel_linux@outlook.com>
2018-09-22 20:36:06 -0500

    Added routes for users.

--- routes/users.js
+++ routes/users.js
@@ -58,4 +58,64 @@ router.put('/:user_id', (req, res) => {
   res.send('editado');
 });
 
+router.delete('/user_id', (req, res) => {
+  res.send(`${req.params.username} : ${req.params.user_id} deleted`,);
+});
+
+//FRIENDS
+
+router.get('/friends', (req, res) => {
+  const users = [
+    {
+      username: 'Juan',
+      name: 'juan',
+      email: 'mail@mail.com',
+    },
+    {
+      username: 'Pedro',
+      name: 'pedro',
+      email: 'mail2@mail2.com',
+    },
+  ];
+  const json = {
+    response: 'Ok',
+    data: users,
+    total: 2,
+  }
+
+  res.send(json);
+});
+
+router.get('/friends/:friend_id', (req, res) => {
+    const json = {
+        response: 'ok',
+        data: {
+            username: `UserName${req.params.user_id}`,
+            name: `Name${req.params.user_id}@mail.com`,
+            mail: `Mail${req.params.user_id}@mail.com`,
+        },
+    };
+    res.send(json);
+});
+
+router.post('/friends/', /*friendsMiddleWare.checkIntegrity*/(req, res) => {
+  console.log(req.body);
+  const json = {
+    response: 'Ok',
+    data: {
+      user_id: 100,
+      name: req.body.name
+    },
+  }
+  res.send(json);
+});
+
+router.delete('/friends/:friend_id', (req, res) => {
+  res.send(`${req.params.username} : ${req.params.user_id} deleted`,);
+})
+
+router.delete('/user_id', (req, res) => {
+  res.send(`${req.params.username} : ${req.params.user_id} deleted`,);
+});
+
 module.exports = router;
