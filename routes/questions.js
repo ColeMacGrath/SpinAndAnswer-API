1b0bbf9d19a6a9463ab80603b8e8dddf6f5004f7
Moisés Córdova <kernel_linux@outlook.com>
2018-10-01 20:15:34 -0500

    Question middleware for post fixed.

--- routes/questions.js
+++ routes/questions.js
@@ -8,6 +8,20 @@ router.get('/:questionId', questionCtrl.get);
 
 router.post('/', questionCtrl.create);
 
+router.post('/', (req, res, next) => {
+    questionMiddleWare.validator.validate(req, res, next, {
+        body: {
+            category: 'required',
+            question: 'word,required',
+            correct_answer: 'word,required',
+            answerOne: 'word,required',
+            answerTwo: 'word,required',
+            answerThree: 'word,required',
+        },
+    });
+}, questionCtrl.create);
+
+
 // Question approval
 router.patch('/:questionId', questionCtrl.changeActive);
 
