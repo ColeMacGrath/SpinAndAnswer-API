91bbb51972446c095b9dfb3afbbc5e0428455347
Moisés Córdova <kernel_linux@outlook.com>
2018-10-08 18:04:06 -0500

    restores app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// const express = require('express');
-// require('dotenv').config();
-//
-// const app = express();
-//
-// const router = require('./routes');
-//
-// app.use(router);
-//
-// app.listen(3000, () => console.log('running'));
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const router = require('./routes');
+
+app.use(router);
+
+app.listen(3000, () => console.log('running'));
 console.log('log');
