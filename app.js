8f1f74b0eea52db544e78d0c0790f004b22b4d0e
Moisés Córdova <kernel_linux@outlook.com>
2018-10-08 19:18:31 -0500

    init

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 
 const router = require('./routes');
 
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
 app.use(router);
 
 app.listen(3000, () => console.log('running'));
-console.log('log');
